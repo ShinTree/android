d8118064cf05a71dc10017d09144e0507fc79113
TreeShin <dngustls@naver.com>
2019-09-20 17:16:59 +0900

    splash (카톡 노랑 + Hello World)

---
+++ TEST/app/src/main/java/com/example/test/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.test;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        startActivity(new Intent(this,MainActivity.class));
+        try{
+            Thread.sleep(5000);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        finish();
+    }
+}
