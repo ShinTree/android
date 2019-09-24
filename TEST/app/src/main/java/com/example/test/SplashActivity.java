16ce33284e5e0e79e53e18bb9cd0c5dede8147c6
TreeShin <dngustls@naver.com>
2019-09-24 13:47:21 +0900

    no message

--- TEST/app/src/main/java/com/example/test/SplashActivity.java
+++ TEST/app/src/main/java/com/example/test/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.example.test;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import java.net.URL;
 
 
 public class SplashActivity extends Activity {
@@ -16,5 +19,28 @@ public class SplashActivity extends Activity {
             e.printStackTrace();
         }
         finish();
+    }
+
+    private class UserThread extends AsyncTask {
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Long doInBackground(URL url){
+            // 전달된 URL 사용 작업
+            return 0;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer progress){
+            // 파일 다운로드 퍼센티지 표시
+        }
+
+        @Override
+        protected void onPostExecute(Long result){
+            // doInBackground에서 받아온 return값 사용
+        }
     }
 }
